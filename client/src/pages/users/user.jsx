eb2f6194edcaa050c3ac523e05059970a8fb6907
muryloandrades <muryloandrades@gmail.com>
2023-04-02 00:12:49 -0300

    remove line

--- client/src/pages/users/user.jsx
+++ client/src/pages/users/user.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Navbar from '../../components/NavBar/navbar'
 import UserTable from '../../components/UserManagement/managentUser'
-// import Config from '../UserManagement/user'
 function User () {
     return(
         <>
