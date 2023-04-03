8bb9bc12b92a56b5ac37183c9122a4f9e092409d
muryloandrades <muryloandrades@gmail.com>
2023-04-03 02:01:45 -0300

    Overflow Concerts

--- client/src/pages/users/user.jsx
+++ client/src/pages/users/user.jsx
@@ -3,7 +3,7 @@ import Navbar from '../../components/NavBar/navbar'
 import UserTable from '../../components/UserManagement/managentUser'
 function User () {
     return(
-        <div style={{width:'100%', height:'100%'}}>
+        <div style={{width:'100%'}}>
             <Navbar />                
             <UserTable />
         </div>
